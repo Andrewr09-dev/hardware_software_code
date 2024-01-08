7f03ace45aa87b90ab0ec72aee37a6cb9842d065
Andrewr09-dev <romeroandrew453@yahoo.com>
2024-01-08 11:52:27 -0500

    first commit

---
+++ Andrew.py
@@ -0,0 +1,12 @@
+def main():
+    print("What is your name?", end ="")
+    answer = input()
+    name = "Andrew Romero"
+    print("We want to know if you like progamming!")
+    print("Do you like programming "+ name + "?", end ="")
+    answer = input()
+    print("Great! You said "'+ answer +'?")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
